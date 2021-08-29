b23f501a67a03963896b6b4c2be35c8edf08f7bb
Plinio Nascimento <plinio.gsnascimento@gmail.com>
2021-08-29 14:50:48 -0300

    Adding tf env

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "client_id" {}
+variable "client_secret" {}
+
+variable "agents_count" {
+    default = 4
+}
+
+variable "max_pods" {
+    default = 100
+}
+
+variable "agents_size" {
+    default = "Standard_D2s_v3"
+}
+
+variable "resource_group_name" {
+    default = "default_groups_name"
+}
+
+variable "location" {
+    default = "Central US"
+}
+
+variable "prefix" {
+    default = "default_prefix"
+}
