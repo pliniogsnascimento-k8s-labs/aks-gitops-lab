f8a320b24f6e127292fe21dc692721c645ff1a41
Plinio Nascimento <plinio.gsnascimento@gmail.com>
2021-08-29 14:52:28 -0300

    Format fix

--- variables.tf
+++ variables.tf
@@ -2,25 +2,25 @@ variable "client_id" {}
 variable "client_secret" {}
 
 variable "agents_count" {
-    default = 4
+  default = 4
 }
 
 variable "max_pods" {
-    default = 100
+  default = 100
 }
 
 variable "agents_size" {
-    default = "Standard_D2s_v3"
+  default = "Standard_D2s_v3"
 }
 
 variable "resource_group_name" {
-    default = "default_groups_name"
+  default = "default_groups_name"
 }
 
 variable "location" {
-    default = "Central US"
+  default = "Central US"
 }
 
 variable "prefix" {
-    default = "default_prefix"
+  default = "default_prefix"
 }
