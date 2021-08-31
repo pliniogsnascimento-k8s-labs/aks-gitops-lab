17779cebbc7a7b0833a75845c04268f1e4516454
Plinio Nascimento <plinio.gsnascimento@gmail.com>
2021-08-31 17:21:56 -0300

    Adding litmus chart

---
+++ litmus.tf
@@ -0,0 +1,17 @@
+resource "kubernetes_namespace" "litmus" {
+  metadata {
+    name = "litmus"
+
+    labels = {
+      "app" = "litmus"
+    }
+  }
+}
+
+resource "helm_release" "litmuschaos" {
+  name = "litmus"
+
+  repository = "https://litmuschaos.github.io/litmus-helm"
+  chart      = "litmuschaos/litmus"
+  namespace  = kubernetes_namespace.litmus.metadata[0].name
+}
